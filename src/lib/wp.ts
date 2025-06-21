1412d78c02b2ac6b84d51f648f5751060d7a1b94
totom3ndez <tomii.mendez45@gmail.com>
2025-06-21 16:24:41 +0200

    fix: correct error messages and improve logging in getVideo and getImage functions

--- src/lib/wp.ts
+++ src/lib/wp.ts
@@ -20,22 +20,47 @@ export const getVideo = async (id: number) => {
     const response = await fetch(`${domain}/wp-json/wp/v2/media/${id}`)
 
     // Log the response status
-    console.log('Response status:', response.status)
-
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`)
     }
 
     const data = await response.json()
-    console.log('API Response:', data) // Log full response
 
     if (!data.source_url) {
-      throw new Error('Video URL not found in response')
+      throw new Error('Image URL not found in response')
     }
 
     const videoUrl = data.source_url
     console.log('Video URL:', videoUrl) // Log the extracted URL
     return videoUrl
+  } catch (error) {
+    console.error('Error fetching image:', error)
+    throw error
+  }
+}
+
+export const getImage = async (id: number) => {
+  try {
+    const response = await fetch(`${domain}/wp-json/wp/v2/media/${id}`)
+
+    // Log the response status
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`)
+    }
+
+    const data = await response.json()
+
+    if (!data.source_url) {
+      throw new Error('Video URL not found in response')
+    }
+
+    const image = {
+      Url: data.source_url,
+      Title: data.title.rendered,
+      Desc: data.caption.rendered
+    }
+
+    return image
   } catch (error) {
     console.error('Error fetching video:', error)
     throw error
