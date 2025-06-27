93ac2c5ad97828b3aad0d04063ec746e57cb7ff9
totom3ndez <tomii.mendez45@gmail.com>
2025-06-27 17:41:51 +0200

    feat: add configuration for Netlify CMS and create new pages for Contact, Projects, and About sections

--- src/components/ui/Card.jsx
+++ src/components/ui/Card.jsx
@@ -1,31 +1,16 @@
-import { createElement, useEffect, useState } from 'react';
-import { getImage } from '@/lib/wp';
 
-const Card = ({ imageID }) => {
-  const [data, setData] = useState({ url: '', title: '', desc: '' });
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const { Url: url, Title: title, Desc: desc } = await getImage(imageID)
-      const tempDiv = document.createElement('div')
-      tempDiv.innerHTML = desc
-      setData({ url, title, desc: tempDiv.innerText });
-    };
-    fetchData();
-  }, [imageID]);
-
-  const { url, title, desc } = data;
+const Card = () => {
 
   return (
     <article className="w-[300px] h-[300px] rounded-t-4xl relative group">
       <img
         className="w-full h-full object-cover rounded-t-4xl"
-        src={url}
+        src=''
         alt="Imagen de flores pintadas en una tela"
       />
       <div className="bg-white rounded-b-4xl h-20 py-5 flex flex-col justify-center items-center">
-        <h3 className="font-headers text-2xl text-center text-black">{title}</h3>
-        <p id="desc" className="text-center text-titles">{desc}</p>
+        <h3 className="font-headers text-2xl text-center text-black">Titulo de tarjeta</h3>
+        <p id="desc" className="text-center text-titles">Descripcion</p>
       </div>
     </article>
   );
